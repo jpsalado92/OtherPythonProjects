9f77e82c6f738374f4231063ce54dd80dde7af5f
jpsalado <jpsalado92@gmail.com>
2019-02-27 16:59:06 +0800

    20190227 Working on Boltzmann machines, integrating NN

--- C02_DL/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Boltzmann_Machines/rbm_mysol.py
+++ C02_DL/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Boltzmann_Machines/rbm_mysol.py
@@ -1,6 +1,5 @@
-# Boltzmann Machines
+# Importing Libraries
 
-# Importing the libraries
 import numpy as np
 import pandas as pd
 import torch
@@ -10,7 +9,7 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 
-# Importing the dataset
+# Importing Dataset
 movies = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
 users = pd.read_csv('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
 ratings = pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python', encoding='latin-1')
@@ -80,40 +79,6 @@ class RBM():
         self.b += torch.sum((v0 - vk), 0)
         self.a += torch.sum((ph0 - phk), 0)
 
+nw = RBM(1,2)
+print('finish')
 
-nv = len(training_set[0])
-nh = 100
-batch_size = 100
-rbm = RBM(nv, nh)
-
-# Training the RBM
-nb_epoch = 10
-for epoch in range(1, nb_epoch + 1):
-    train_loss = 0
-    s = 0.
-    for id_user in range(0, nb_users - batch_size, batch_size):
-        vk = training_set[id_user:id_user + batch_size]
-        v0 = training_set[id_user:id_user + batch_size]
-        ph0, _ = rbm.sample_h(v0)
-        for k in range(10):
-            _, hk = rbm.sample_h(vk)
-            _, vk = rbm.sample_v(hk)
-            vk[v0 < 0] = v0[v0 < 0]
-        phk, _ = rbm.sample_h(vk)
-        rbm.train(v0, vk, ph0, phk)
-        train_loss += torch.mean(torch.abs(v0[v0 >= 0] - vk[v0 >= 0]))
-        s += 1.
-    print('epoch: ' + str(epoch) + ' loss: ' + str(train_loss / s))
-
-# Testing the RBM
-test_loss = 0
-s = 0.
-for id_user in range(nb_users):
-    v = training_set[id_user:id_user + 1]
-    vt = test_set[id_user:id_user + 1]
-    if len(vt[vt >= 0]) > 0:
-        _, h = rbm.sample_h(v)
-        _, v = rbm.sample_v(h)
-        test_loss += torch.mean(torch.abs(vt[vt >= 0] - v[vt >= 0]))
-        s += 1.
-print('test loss: ' + str(test_loss / s))
